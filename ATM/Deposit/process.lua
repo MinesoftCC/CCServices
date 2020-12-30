2fd53ef515f522b1a57bce2709dc8777759e20a8
William Katz <qualitycontentquestionmark@gmail.com>
2020-12-29 21:25:52 -0800

    :bug: if 0 it crashed, also formatted code

--- ATM/Deposit/process.lua
+++ ATM/Deposit/process.lua
@@ -1,51 +1,57 @@
 while true do
-local ip = "http://127.0.0.1/BankAPI/sendfunds/atm/" 
-local atm_pass = "atmpass123"
-local atm_id = 2
-local json = require("json")
-while not redstone.getInput("back") do os.sleep(0.1) end
-
-local file = fs.open("disk/order.txt", "r")
-local account_name = file.readAll()
-file.close()
-
---grab chest contents
-while turtle.suckUp() do end
-
---if diamonds add total otherwise spit out 
-local total = 0
-for i=1,15 do
-    turtle.select(i)
-    if turtle.compareTo(16) then
-        total = total + turtle.getItemCount()
-    else
-        turtle.drop()
+    local ip = "http://127.0.0.1/BankAPI/sendfunds/atm/"
+    local atm_pass = "atmpass123"
+    local atm_id = 2
+    local json = require("json")
+    while not redstone.getInput("back") do
+        os.sleep(0.1)
     end
-end
 
---attempt to send money
-local res = json.decode(http.post(ip..account_name.."/"..total.."/"..atm_id.."/"..atm_pass, ""):readAll())
+    local file = fs.open("disk/order.txt", "r")
+    local account_name = file.readAll()
+    file.close()
 
---if it works then deposit diamonds
-if res["value"] == 1 then
-    turtle.turnRight()
-    turtle.turnRight()
-    for i=1,15 do
+    --grab chest contents
+    while turtle.suckUp() do
+    end
+
+    --if diamonds add total otherwise spit out
+    local total = 0
+    for i = 1, 15 do
         turtle.select(i)
-        turtle.dropDown()
+        if turtle.compareTo(16) then
+            total = total + turtle.getItemCount()
+        else
+            turtle.drop()
+        end
     end
-    turtle.turnLeft()
-    turtle.turnLeft()
-    
-    --notification
-    redstone.setOutput("right", true)
-    os.sleep(1)
-    redstone.setOutput("right", false)
-    --money has been deposited
-else
-    for i=1,15 do 
-        turtle.select(i)
-        turtle.drop() 
+
+    --attempt to send money
+    if total ~= 0 then
+        local res =
+            json.decode(http.post(ip .. account_name .. "/" .. total .. "/" .. atm_id .. "/" .. atm_pass, ""):readAll())
+
+        --if it works then deposit diamonds
+        if res["value"] == 1 then
+            --money has been deposited
+            turtle.turnRight()
+            turtle.turnRight()
+            for i = 1, 15 do
+                turtle.select(i)
+                turtle.dropDown()
+            end
+            turtle.turnLeft()
+            turtle.turnLeft()
+
+            --notification
+            redstone.setOutput("right", true)
+            os.sleep(1)
+            redstone.setOutput("right", false)
+        else
+            for i = 1, 15 do
+                turtle.select(i)
+                turtle.drop()
+            end
+        end
     end
-end
 end
