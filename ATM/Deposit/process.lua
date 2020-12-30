adc27691ab97185822de752a008a1eabb3963757
William Katz <qualitycontentquestionmark@gmail.com>
2020-12-29 16:11:41 -0800

    :bug: didnt spit out diamonds if account didnt exist

--- ATM/Deposit/process.lua
+++ ATM/Deposit/process.lua
@@ -1,6 +1,6 @@
 while true do
-local ip = "http://127.0.0.1/BankAPI/sendfunds/atm/" 
-local atm_pass = "atmpass123"
+local ip = "http://157.90.30.90/BankAPI/sendfunds/atm/" 
+local atm_pass = "atmbeepboop"
 local atm_id = 2
 local json = require("json")
 while not redstone.getInput("back") do os.sleep(0.1) end
@@ -27,7 +27,8 @@ end
 local res = json.decode(http.post(ip..account_name.."/"..total.."/"..atm_id.."/"..atm_pass, ""):readAll())
 
 --if it works then deposit diamonds
-if res["value"] then
+if res["value"] == 1 then
+    print("was sent")
     turtle.turnRight()
     turtle.turnRight()
     for i=1,15 do
@@ -42,5 +43,10 @@ if res["value"] then
     os.sleep(1)
     redstone.setOutput("right", false)
     --money has been deposited
+else
+    for i=1,15 do 
+        turtle.select(1)
+        turtle.drop() 
+    end
 end
 end
