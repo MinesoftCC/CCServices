7f1a734642272ad56947f3d817b39e670cd1e8d1
EntireTwix <willkatz.dev@gmail.com>
2021-01-01 09:01:00 -0800

    LazySave removed

--- LazySave/save.lua
+++
@@ -1,8 +0,0 @@
-local ip = "http://127.0.0.1/BankAPI/admin/save/pass"
-while true do
-    if redstone.getInput("top") then
-        http.post(ip,"")
-        os.sleep(1)
-    end 
-    os.sleep(0.1)
-end
