2e0167958a415c13f6cf9724b630b0aa3eb9343d
EntireTwix <willkatz.dev@gmail.com>
2021-01-03 08:15:51 -0800

    :bug: invalid input crashed bug

--- ATM/Withdraw/process.lua
+++ ATM/Withdraw/process.lua
@@ -9,11 +9,17 @@ while true do
     local args = textutils.unserialise(file.readAll())
     file.close()
     
-    local resp = json.decode(http.post(ip..args[1].."/"..atm_account.."/"..args[2].."/"..args[3].."/"..args[4],""):readAll())
-    if resp["value"] == 1 and tonumber(args[2]) then
-        redstone.setOutput("right", true)
-        os.sleep(tonumber(args[2])/2.5)
-        redstone.setOutput("right", false)
+    local res = false
+    for i=1,4 do
+        if args[i] == nil then res = true end
+    end
+    if not res then
+        local resp = json.decode(http.post(ip..args[1].."/"..atm_account.."/"..args[2].."/"..args[3].."/"..args[4],""):readAll())
+        if resp["value"] == 1 and tonumber(args[2]) then
+            redstone.setOutput("right", true)
+            os.sleep(tonumber(args[2])/2.5)
+            redstone.setOutput("right", false)
+        end
     end
     while redstone.getInput("back") do os.sleep(0.1) end
 end
